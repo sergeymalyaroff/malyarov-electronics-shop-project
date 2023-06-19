90ddf20350cd0713ad33d8bcb1a44650c70b4328
Sergey Malyarov <sergey@malyaroff.com>
2023-06-19 12:38:48 +0300

    hw-1

--- src/item.py
+++ src/item.py
@@ -2,8 +2,8 @@ class Item:
     """
     Класс для представления товара в магазине.
     """
-    pay_rate = 1.0
-    all = []
+    pay_rate = 1.0  # уровень цен с учетом скидки
+    all = []  # список для хранения всех созданных экземпляров
 
     def __init__(self, name: str, price: float, quantity: int) -> None:
         """
@@ -13,7 +13,12 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        pass
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+
+        # добавление нового экземпляра в список
+        self.__class__.all.append(self)
 
     def calculate_total_price(self) -> float:
         """
@@ -21,10 +26,16 @@ class Item:
 
         :return: Общая стоимость товара.
         """
-        pass
+        return self.price * self.quantity
 
     def apply_discount(self) -> None:
         """
         Применяет установленную скидку для конкретного товара.
         """
-        pass
+        self.price = self.price * self.__class__.pay_rate
+
+    def __repr__(self):
+        """
+        Возвращает строковое представление экземпляра класса Item.
+        """
+        return f"Item('{self.name}', {self.price}, {self.quantity})"
--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,28 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+
+import pytest
+from src.item import Item
+
+def test_item_creation():
+    item = Item("Смартфон", 10000, 20)
+    assert item.name == "Смартфон"
+    assert item.price == 10000
+    assert item.quantity == 20
+
+def test_calculate_total_price():
+    item = Item("Смартфон", 10000, 20)
+    assert item.calculate_total_price() == 200000
+
+def test_apply_discount():
+    Item.pay_rate = 0.8  # устанавливаем новый уровень цен
+    item = Item("Смартфон", 10000, 20)
+    item.apply_discount()
+    assert item.price == 8000.0
+
+def test_all_items_list():
+    Item.all = []  # очищаем список перед тестом
+    item1 = Item("Смартфон", 10000, 20)
+    item2 = Item("Ноутбук", 20000, 5)
+    assert item1 in Item.all
+    assert item2 in Item.all
+
